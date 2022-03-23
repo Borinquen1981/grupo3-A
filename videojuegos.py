ed85fbbaa4747de4dc522350c4577651508d5420
Carlos Hidalgo <cahidalgolu@uide.edu.ec>
2022-03-22 21:04:46 -0500

    Correccion catalogo2

--- videojuegos.py
+++ videojuegos.py
@@ -1,5 +1,7 @@
 videojuegos=[
     {"codigo":1,"name": "mario","precio": 80,"cantidad": 10, "consola":"ps4"},
     { "codigo":2,"name": "zelda","precio": 50,"cantidad": 8, "consola":"nintendo"},
-    {"codigo":3,"name": "metroide","precio": 60,"cantidad": 7, "consola":"xbox"}
+    {"codigo":3,"name": "metroide","precio": 60,"cantidad": 7, "consola":"xbox"},
+    {"codigo":4,"name": "wrc-2021","precio": 60,"cantidad": 7, "consola":"ps4"},
+    {"codigo":5,"name": "fifa2022","precio": 60,"cantidad": 7, "consola":"xbox"}
 ]
