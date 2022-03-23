d48d7cda3c65d5f10742e7de7c9783d36acebbdf
Borinquen1981 <101934503+Borinquen1981@users.noreply.github.com>
2022-03-22 21:03:12 -0500

    Update videojuegos.py
    Es una nueva busqueda

--- videojuegos.py
+++ videojuegos.py
@@ -2,4 +2,5 @@ videojuegos=[
     {"codigo":1,"name": "mario","precio": 80,"cantidad": 10, "consola":"ps4"},
     { "codigo":2,"name": "zelda","precio": 50,"cantidad": 8, "consola":"nintendo"},
     {"codigo":3,"name": "metroide","precio": 60,"cantidad": 7, "consola":"xbox"}
+    {"codigo":4,"name": "mansionluigui","precio": 50,"cantidad": 10, "consola":"ps5"}
 ]
